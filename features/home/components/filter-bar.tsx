1184c0a6bea312434096ead55a10a9782ba2f1d4
Jerome Doran <disneyfan9184@gmail.com>
2025-01-31 16:21:21 -0500

    generated sanity types

--- features/home/components/filter-bar.tsx
+++ features/home/components/filter-bar.tsx
@@ -16,10 +16,10 @@ export default function FilterBar({
       <div className='flex items-center gap-2'>
         {productType?.map((tab) => (
           <button
-            key={tab.title}
-            onClick={() => setSelectedTab(tab?.title)}
+            key={tab.value}
+            onClick={() => setSelectedTab(tab?.value)}
             className={`border border-maestroDark px-4 py-1.5 md:px-6 md:py-2 rounded-full hover-effect hover:bg-maestroDark hover:text-white ${
-              selectedTab === tab?.title && 'bg-maestroDark text-white'
+              selectedTab === tab?.value && 'bg-maestroDark text-white'
             }`}
           >
             {tab.title}
--- features/home/components/product-grid.tsx
+++ features/home/components/product-grid.tsx
@@ -1,15 +1,52 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import FilterBar from './filter-bar';
 import { productType } from '@/constants';
+import { client } from '@/sanity/lib/client';
+import { Product } from '@/sanity.types';
 
 export default function ProductGrid() {
   const [selectedTab, setSelectedTab] = useState(productType[0]?.title || '');
-  console.log(selectedTab);
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const query = `*[_type == 'product' && variant == $variant] | order(name asc)`;
+  const params = {
+    variant: selectedTab.toLocaleLowerCase(),
+  };
+
+  useEffect(() => {
+    async function fetchProducts() {
+      setLoading(true);
+      try {
+        const response = await client.fetch(query, params);
+        const data = await response;
+        setProducts(data);
+
+        setLoading(false);
+      } catch (error) {
+        console.log('Product fetch error:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProducts();
+  }, [selectedTab]);
+
   return (
     <div className='mt-10 flex items-center flex-col'>
       <FilterBar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
+      {loading ? (
+        <div>
+          <span>Product is loading</span>
+        </div>
+      ) : (
+        products?.map((product: Product) => (
+          <p key={product?._id}>{product?.name}</p>
+        ))
+      )}
     </div>
   );
 }
