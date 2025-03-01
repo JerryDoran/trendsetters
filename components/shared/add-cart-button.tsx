c5ae0dd5ac7b86cf758a7174bb6bc38cdc83ac97
Jerome Doran <disneyfan9184@gmail.com>
2025-03-01 09:25:01 -0500

    added functionality to conditionally style out of stock items

--- components/shared/add-cart-button.tsx
+++ components/shared/add-cart-button.tsx
@@ -11,9 +11,12 @@ export default function AddToCartButton({
   product,
   className,
 }: ProductCardProps) {
+  const isOutOfStock = product?.stock === 0;
+
   return (
     <div>
       <Button
+        disabled={isOutOfStock}
         variant='outline'
         className={cn(
           'w-full bg-transparent hover:bg-maestroDark hover:text-white hover-effect font-semibold text-sm tracking-wide',
--- components/shared/product-card.tsx
+++ components/shared/product-card.tsx
@@ -12,8 +12,8 @@ type ProductCardProps = {
 export default function ProductCard({ product }: ProductCardProps) {
   console.log(product);
   return (
-    <div className='group text-sm rounded-lg overflow-hidden'>
-      <div className='bg-gradient-to-r from-zinc-200 via-zinc-300 to-zinc-400 h-72 overflow-hidden'>
+    <div className='group text-sm rounded-lg overflow-hidden w-[300px] h-[500px]'>
+      <div className='bg-gradient-to-r from-zinc-200 via-zinc-300 to-zinc-400 h-72 overflow-hidden relative'>
         {product?.images && (
           <Link href='/product'>
             <Image
@@ -22,12 +22,17 @@ export default function ProductCard({ product }: ProductCardProps) {
               height={500}
               alt={product.name || 'Product'}
               priority
-              className={`w-full h-full object-fill overflow-hidden group-hover:scale-105 hover-effect`}
+              className={`w-full h-full object-fill overflow-hidden  hover-effect ${product?.stock === 0 ? 'opacity-80' : 'group-hover:scale-105'}`}
             />
           </Link>
         )}
+        {product?.stock === 0 && (
+          <div className='absolute top-0 left-0 w-full h-full bg-maestroDark/60 flex items-center justify-center'>
+            <p className='text-base text-white font-semibold'>Out of stock</p>
+          </div>
+        )}
       </div>
-      <div className='py-3 px-2 flex flex-col gap-1.5 bg-zinc-50 border border-t-0 rounded-b-lg'>
+      <div className='py-3 px-2 flex flex-col gap-2 bg-zinc-50 border border-t-0 rounded-b-lg h-42'>
         <h2 className='font-semibold line-clamp-1'>{product.name}</h2>
         <p className=''>{product?.intro}</p>
         <PriceView
